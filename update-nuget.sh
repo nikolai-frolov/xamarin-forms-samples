b05781183a491f32738257b4b618b662c23f542b
MaksimZhukov <46996400+MaksimZhukov@users.noreply.github.com>
2021-09-21 19:09:57 +0300

    Update update-nuget.sh

--- update-nuget.sh
+++ update-nuget.sh
@@ -1,3 +1,40 @@
+MONO_VERSIONS_PATH='/Library/Frameworks/Mono.framework/Versions'
+TMPMOUNT=`/usr/bin/mktemp -d /tmp/visualstudio.XXXX`
+
+download_with_retries() {
+# Due to restrictions of bash functions, positional arguments are used here.
+# In case if you using latest argument NAME, you should also set value to all previous parameters.
+# Example: download_with_retries $ANDROID_SDK_URL "." "android_sdk.zip"
+    local URL="$1"
+    local DEST="${2:-.}"
+    local NAME="${3:-${URL##*/}}"
+    local COMPRESSED="$4"
+
+    if [[ $COMPRESSED == "compressed" ]]; then
+        COMMAND="curl $URL -4 -sL --compressed -o '$DEST/$NAME'"
+    else
+        COMMAND="curl $URL -4 -sL -o '$DEST/$NAME'"
+    fi
+
+    echo "Downloading $URL..."
+    retries=20
+    interval=30
+    while [ $retries -gt 0 ]; do
+        ((retries--))
+        eval $COMMAND
+        if [ $? != 0 ]; then
+            echo "Unable to download $URL, next attempt in $interval sec, $retries attempts left"
+            sleep $interval
+        else
+            echo "$URL was downloaded successfully to $DEST/$NAME"
+            return 0
+        fi
+    done
+
+    echo "Could not download $URL"
+    return 1
+}
+
 installNuget() {
   local MONO_VERSION=$1
   local NUGET_VERSION=$2
