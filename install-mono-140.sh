095be2f7beec150aad54ecbf8e2f8d23b542861c
MaksimZhukov <46996400+MaksimZhukov@users.noreply.github.com>
2021-09-21 18:42:35 +0300

    Update install-mono-140.sh

--- install-mono-140.sh
+++ install-mono-140.sh
@@ -84,11 +84,27 @@ installMono() {
   sudo mv -v $MONO_VERSIONS_PATH/* $TMPMOUNT_FRAMEWORKS/mono/
 }
 
-pushd $TMPMOUNT
+installNuget() {
+  local MONO_VERSION=$1
+  local NUGET_VERSION=$2
+  local NUGET_URL="https://dist.nuget.org/win-x86-commandline/v${NUGET_VERSION}/nuget.exe"
+  echo "Installing nuget $NUGET_VERSION for Mono $MONO_VERSION"
+  cd ${MONO_VERSIONS_PATH}/${MONO_VERSION}/lib/mono/nuget
+  sudo mv nuget.exe nuget_old.exe
 
+  pushd $TMPMOUNT
+  download_with_retries $NUGET_URL "." "nuget.exe"
+  sudo chmod a+x nuget.exe
+  sudo mv nuget.exe ${MONO_VERSIONS_PATH}/${MONO_VERSION}/lib/mono/nuget
+  popd
+}
+
+pushd $TMPMOUNT
 
 installMono $MONO_VERSION
 sudo mv -v $TMPMOUNT_FRAMEWORKS/mono/* $MONO_VERSIONS_PATH/
-
+installNuget "6.12.0" "5.9.1"
 
 popd
+
+installNuget "6.12.0" "5.9.1"
